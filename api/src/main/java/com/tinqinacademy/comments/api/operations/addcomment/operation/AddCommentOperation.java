78a950d634a66e14f3b2ae16a6baca394c8b55b6
Viktor Denchev <87856468+VDenchev@users.noreply.github.com>
2024-08-02 12:26:53 +0300

    [TCA-5] Rename package from createcomment to addcomment

---
+++ api/src/main/java/com/tinqinacademy/comments/api/operations/addcomment/operation/AddCommentOperation.java
@@ -0,0 +1,8 @@
+package com.tinqinacademy.comments.api.operations.addcomment.operation;
+
+import com.tinqinacademy.comments.api.base.Operation;
+import com.tinqinacademy.comments.api.operations.addcomment.input.AddCommentInput;
+import com.tinqinacademy.comments.api.operations.addcomment.output.AddCommentOutput;
+
+public interface AddCommentOperation extends Operation<AddCommentInput, AddCommentOutput> {
+}
