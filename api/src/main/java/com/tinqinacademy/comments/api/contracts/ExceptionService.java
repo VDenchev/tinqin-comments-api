c1e5fe4e61f33d358bf661b191f3ae1e1a47ea73
Viktor Denchev <87856468+VDenchev@users.noreply.github.com>
2024-08-06 11:33:53 +0300

    [TCA-19] Remove unused classes

--- api/src/main/java/com/tinqinacademy/comments/api/contracts/ExceptionService.java
+++
@@ -1,8 +0,0 @@
-package com.tinqinacademy.comments.api.contracts;
-
-import com.tinqinacademy.comments.api.models.exceptions.output.ErrorOutput;
-import org.springframework.http.HttpStatusCode;
-
-public interface ExceptionService {
-  ErrorOutput processException(Exception ex, HttpStatusCode statusCode);
-}
